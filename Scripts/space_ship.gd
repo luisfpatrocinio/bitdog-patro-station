45954025a245fdb4a7935865446fd06b3555df79
Luis Felipe Patrocinio <patrocinioluisf@gmail.com>
2025-04-22 09:35:15 -0300

    feat: added ship movement limit

--- Scripts/space_ship.gd
+++ Scripts/space_ship.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 @onready var mainScreen = get_tree().get_first_node_in_group("MainScreen") as MainScreen;
 var velX: float;
 var velY: float;
+var startPosition: Vector2;
+@export var moveRange: float = 100.0;
+
+func _ready() -> void:
+	startPosition = position;
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(velX * 10.0, velY * 10.0);
@@ -10,5 +15,7 @@ func _physics_process(delta: float) -> void:
 	if velocity.length() > 0:
 		rotation = lerp_angle(rotation, velocity.angle(), 0.20);
 	move_and_slide();
-	pass
 	
+	# Limitar posição da nave
+	position.x = clamp(position.x, startPosition.x - moveRange, startPosition.x + moveRange);
+	position.y = clamp(position.y, startPosition.y - moveRange, startPosition.y + moveRange);
